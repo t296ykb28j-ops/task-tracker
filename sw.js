c96011a85a2826ab86da4ed265d155f3c63111f4
t296ykb28j-ops <t296ykb28j@privaterelay.appleid.com>
2026-07-07 08:42:52 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "life-tracker-v2";
+const CACHE = "life-tracker-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 
 self.addEventListener("install", (e) => {
